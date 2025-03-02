ffd0c69df45bf39439d6c69f03d94ecbf34fc8a8
H0ngJu <hongju4385@gmail.com>
2025-03-03 00:27:49 +0900

    Updates

--- assets/index-BBsu1vWA.js
+++ assets/index-BBsu1vWA.js
@@ -162,7 +162,7 @@ const showLotto = (lotto) => {
   container.id = "lotto";
   const img = new Image();
   img.id = "lotto-image";
-  img.src = "../web/images/lotto.png";
+  img.src = "/images/lottoImage.png";
   img.alt = "lotto-image";
   const numbersDiv = document.createElement("div");
   numbersDiv.innerHTML = lotto.getNumbers().join(", ");
